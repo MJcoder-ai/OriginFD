2f9a13c7512ec795ac3494e75eca85e738b9c7d9
MJ Coder <76821733+MJcoder-ai@users.noreply.github.com>
2025-09-09 07:26:38 +0100

    feat(web): add shared lib utilities

---
+++ apps/web/src/lib/api-client.ts
@@ -0,0 +1,6 @@
+import client from '@originfd/http-client'
+
+export const apiClient: any = client as any
+export const componentAPI: any = client as any
+export * from '@originfd/http-client'
+export default client
