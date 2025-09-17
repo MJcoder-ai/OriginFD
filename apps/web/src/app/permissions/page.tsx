8854628979bc53829c6d32c07fe99b749e5fba8d
MJ Coder <76821733+MJcoder-ai@users.noreply.github.com>
2025-09-17 14:33:46 +0100

    feat(web): add permission visualizer component

---
+++ apps/web/src/app/permissions/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import PermissionVisualizer from '@/components/permissions/permission-visualizer'
+
+export default function PermissionsPage() {
+  return (
+    <div className="p-4">
+      <PermissionVisualizer />
+    </div>
+  )
+}
+
